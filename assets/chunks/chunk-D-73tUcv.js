821824ea0d5da7dab51ab5a30b6c07353d7fce4d
suliemandev <suleiman@averotech.com>
2026-06-17 15:39:35 +0000

    Deploying to gh-pages from @ suliemandev/vike-fork@3518a2957bca37fa83a6bbf4b98991350df50e8c 🚀

---
+++ assets/chunks/chunk-D-73tUcv.js
@@ -0,0 +1,1 @@
+import{d as s,j as e}from"./chunk-CcgYQrw7.js";function i({prop:t}){s(!t.startsWith("import")),s(!t.startsWith("."));const r="import.meta."+t;return e.jsx("code",{children:r})}export{i as I};
